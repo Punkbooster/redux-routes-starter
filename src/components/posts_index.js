2e09753ea890fa390c449c1ac336cf53d2ae0c53
Punkbooster <ugliar.arsen@gmail.com>
2017-11-22 22:06:35 +0100

    First component route definition

---
+++ src/components/posts_index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class PostsIndex extends Component {
+  render() {
+    return(
+      <div>
+        Index Post file
+      </div>
+    )
+  }
+}
+
+export default PostsIndex
