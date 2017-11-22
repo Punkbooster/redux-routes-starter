dd8c77b00bb6cf7891e12c32b843650914f4a43b
Punkbooster <ugliar.arsen@gmail.com>
2017-11-22 22:34:04 +0100

    Add sample navigation with the link tag

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 class PostsIndex extends Component {
   render() {
     return(
       <div>
-        Index Post file
+        <div className='text-xs-right'>
+          <Link className='btn btn-primary' to='/posts/new'>
+            Add a post
+          </Link>
+        </div>
+        <h3>Index Post Page</h3>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import reducers from './reducers';
 import PostsIndex from './components/posts_index'
+import PostsNew from './components/posts_new'
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
@@ -13,7 +14,11 @@ ReactDOM.render(
   <Provider store={createStoreWithMiddleware(reducers)}>
     <BrowserRouter>
       <div>
-        <Route path='/' component={PostsIndex} />
+        {/* Place most specific routes at the top of the Switch tag */}
+        <Switch>
+          <Route path='/posts/new' component={PostsNew} />
+          <Route path='/' component={PostsIndex} />
+        </Switch>
       </div>
     </BrowserRouter>
   </Provider>
